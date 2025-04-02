cd3ba847e07b5de33b6215bf3df335fdc8cdebbf
Basem <basemxahmed@gmail.com>
2025-04-02 15:11:48 +0400

    Setup tab bar controller with search and favorites tabs, added app icon and image assets

---
+++ GHBook/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GHBook
+//
+//  Created by Basem Elkady on 4/2/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
