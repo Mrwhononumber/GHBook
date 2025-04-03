5c7360834076943f6d3186817707a33445d44f0a
Basem <basemxahmed@gmail.com>
2025-04-03 13:21:39 +0400

    Refactor networking error messages

---
+++ GHBook/Utilities/ErrorMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorMessage.swift
+//  GHBook
+//
+//  Created by Basem Elkady on 4/3/25.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    
+    case invalidUsername = "this username created invalid request. please try again"
+    case unableToComplete = "Unable to complete request. check your internet connection"
+    case invalidResponse = "Invalid response from the server. please try again"
+    case invalidData = "The data received from the server is invalid. please try again"
+    
+    
+}
