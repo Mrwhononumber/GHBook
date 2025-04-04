716d99231732b73e87559ecc76cf63fd34123f05
Basem <basemxahmed@gmail.com>
2025-04-04 16:27:16 +0400

    Create GFEmptyStateView and implement it in FollowerListVC for no followers state

--- GHBook/Screens/FollowerListVC.swift
+++ GHBook/Screens/FollowerListVC.swift
@@ -64,6 +64,12 @@ class FollowerListVC: UIViewController {
                             }
                 if followers.count < 100 { self.hasMoreFollowers = false}
                 self.followers.append(contentsOf: followers)
+                if followers.isEmpty {
+                    let message = " This user has no followers 😢"
+                  DispatchQueue.main.async {self.showEmptyStateView(with: message, in: self.view)}
+                  
+                    return
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff", message: error.rawValue, buttonTitle: "OK")
