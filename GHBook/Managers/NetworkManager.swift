ef4f89f8f3f6fa8630c314d1dbb19492f6d9c310
Basem <basemxahmed@gmail.com>
2025-04-04 10:42:26 +0400

    download, cache, and set avatar image in FollowerListVC

--- GHBook/Managers/NetworkManager.swift
+++ GHBook/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Basem Elkady on 4/3/25.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseUrl = "https://api.github.com/users/"
+    private let baseUrl = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
---
+++ GHBook/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  GHBook
+//
+//  Created by Basem Elkady on 4/3/25.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView ) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width // The whole screen width
+        let padding: CGFloat = 12
+        let minimumItemSpacing:CGFloat = 10 // space between cells
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40 )
+        
+        return flowLayout
+        
+    }
+}
