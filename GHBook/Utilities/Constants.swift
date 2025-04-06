e7345cb87c60aeb5856846274a55d39c719d01d0
Basem <basemxahmed@gmail.com>
2025-04-06 18:03:56 +0400

    Create GFUserInfoHeaderVC and integrate it as a child VC in UserInfoVC

---
+++ GHBook/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHBook
+//
+//  Created by Basem Elkady on 4/6/25.
+//
+
+import Foundation
+
+enum SFSymbols {
+    
+    static let location = "mappin.and.ellipse"
+}
