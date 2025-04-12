b3be88e2508bce0d8775c195a82479e0dce3eea1
Basem <basemxahmed@gmail.com>
2025-04-12 11:35:46 +0400

    Organize GFButton for clarity, add documentation

--- GHBook/Custom Views/Buttons/GFButton.swift
+++ GHBook/Custom Views/Buttons/GFButton.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class GFButton: UIButton {
+    
+    // MARK: - Initializers
 
+
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -25,15 +30,22 @@ class GFButton: UIButton {
         configure()
     }
     
+    
+    
+    // MARK: - Configuration
+    
+    
+    /// Sets up the button's appearance and layout constraints
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        self.setTitleColor(.white, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
-    func set(backgroundColr: UIColor, title: String) {
-        self.backgroundColor = backgroundColr
+    
+    func set(backgroundColor: UIColor, title: String) {
+        self.backgroundColor = backgroundColor
         setTitle(title, for: .normal)
     }
 }
