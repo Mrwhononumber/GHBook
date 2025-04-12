53d4cdc72950b7ae72d7ea9b24a34d30f951745a
Basem <basemxahmed@gmail.com>
2025-04-12 14:48:28 +0400

    Cleanup GFItemInfoVC, Add documentation comments

--- GHBook/Custom Views/ViewControlles/GFItemInfoVC.swift
+++ GHBook/Custom Views/ViewControlles/GFItemInfoVC.swift
@@ -7,29 +7,53 @@
 
 import UIKit
 
+// MARK: - Delegate Protocol
+
+
 protocol GFItemVCDelegate: AnyObject {
     
     func didTapGitHubProfile(for user: User)
     func didTapGetFollowers(for user: User)
 }
 
+
+// MARK: - Reusable Base Class
+
+
 class GFItemInfoVC: UIViewController {
+    
+    
+    // MARK: - UI Elements
+    
     
     let stackView = UIStackView()
     let itemInfoOne = GFItemInfoView()
     let itemInfoTwo = GFItemInfoView()
     let actionButton = GFButton()
+    
+    
+    // MARK: - Properties
+    
+    
     var user: User!
     weak var delegate: GFItemVCDelegate!
+    
+    
+    // MARK: - Initializers
+
     
     init(user: User!) {
         super.init(nibName: nil, bundle: nil )
         self.user = user
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+   
+    // MARK: - Lifecycle
     
     
     override func viewDidLoad() {
@@ -40,25 +64,23 @@ class GFItemInfoVC: UIViewController {
         configureActionButton()
     }
     
+   
+    // MARK: - Configuration
+    
+    
    private func configureBackgroundView() {
         view.layer.cornerRadius = 18
         view.backgroundColor = .secondarySystemBackground
     }
     
-    private func configureStackView() {
+    
+   private func configureStackView() {
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
         
         stackView.addArrangedSubview(itemInfoOne)
         stackView.addArrangedSubview(itemInfoTwo)
     }
-    
-    
-    private func configureActionButton() {
-        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
-    }
-    
-    @objc func actionButtonTapped() {}
     
     
     private func layoutUI() {
@@ -81,4 +103,15 @@ class GFItemInfoVC: UIViewController {
             actionButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
+    
+    
+    private func configureActionButton() {
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    }
+    
+    
+    // MARK: - Actions
+    
+    /// To be overridden by subclasses for custom action behavior
+    @objc func actionButtonTapped() {}
 }
