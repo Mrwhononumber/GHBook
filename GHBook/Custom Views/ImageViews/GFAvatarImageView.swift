11895ca08e0184617a8d590416a2337be9668234
Basem <basemxahmed@gmail.com>
2025-04-12 12:51:28 +0400

    Cleanup GFAvatarImageView, add documentation comments

--- GHBook/Custom Views/ImageViews/GFAvatarImageView.swift
+++ GHBook/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -9,26 +9,40 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    //MARK: - Properties
+    
     let cache = NetworkManager.shared.cache
     let placeHolderImage = UIImage(named: "avatar-placeholder")
 
+    
+    //MARK: - Initializers
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
         
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    //MARK: - Configuration
+    
     private func configure(){
         layer.cornerRadius = 10
         clipsToBounds = true
-        image = placeHolderImage
+        image = placeHolderImage  // Set a default image while downloading the avatar image, and if the download fails
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    
+    //MARK: - Image Downloading
+    
+    /// Downloads the avatar image from a URL string, with caching.
     func downloadImage(from urlString: String) {
         
         let cacheKey = NSString(string: urlString)
@@ -38,12 +52,11 @@ class GFAvatarImageView: UIImageView {
             return
         }
         
-        
         guard let url = URL(string: urlString) else { return }
         
         let task = URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
             guard let self = self else { return }
-            if error != nil { return }
+            if error != nil { return } /// no need to handle errors visibly, we fall back to placeholder
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
             guard let data = data else { return }
            
@@ -53,11 +66,7 @@ class GFAvatarImageView: UIImageView {
             DispatchQueue.main.async {
                 self.image = image
             }
-            
-             
-        
         }
         task.resume()
     }
-    
 }
