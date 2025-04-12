542f2c7b74903a571ecdea857a76841cb245d030
Basem <basemxahmed@gmail.com>
2025-04-12 13:13:31 +0400

    Cleanup GFSecondaryTitleLabel, add documentation comments

--- GHBook/Custom Views/Lables/GFSecondaryTitleLabel.swift
+++ GHBook/Custom Views/Lables/GFSecondaryTitleLabel.swift
@@ -9,29 +9,36 @@ import UIKit
 
 class GFSecondaryTitleLabel: UILabel {
 
+    
+    //MARK: - Initializers
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
-        
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /// Initializes the label with a specific font size
     init(fontSize:CGFloat) {
         super.init(frame: .zero)
         font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
         configure()
     }
     
+    
+    //MARK: - Configuration
+    
+    
     private func configure() {
-        
         textColor = .secondaryLabel
         adjustsFontSizeToFitWidth = true
         minimumScaleFactor = 0.9
         lineBreakMode = .byTruncatingTail
         translatesAutoresizingMaskIntoConstraints = false
     }
-
 }
