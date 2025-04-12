c86d20499f478fb4df7ba02bc4fe70b162efbdbf
Basem <basemxahmed@gmail.com>
2025-04-12 11:17:24 +0400

    Clean up GFError cases and descriptions

--- GHBook/Utilities/GFError.swift
+++ GHBook/Utilities/GFError.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
+
+/// Custom app-wide error types for handling common failure cases
 enum GFError: String, Error {
     
-    case invalidUsername = "this username created invalid request. please try again"
-    case unableToComplete = "Unable to complete request. check your internet connection"
-    case invalidResponse = "Invalid response from the server. please try again"
-    case invalidData = "The data received from the server is invalid. please try again"
-    case unableToFavorite = "Unable to favorite this user. please try again"
-    case alreadyInFavorites = "This user already in favorites"
-    
-    
+    case invalidUsername    = "this username created invalid request. please try again"
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. please try again"
+    case invalidData        = "The data received from the server is invalid. please try again"
+    case unableToFavorite   = "Unable to add this user to favorites. Please try again"
+    case alreadyInFavorites = "This user is already in favorites"
 }
