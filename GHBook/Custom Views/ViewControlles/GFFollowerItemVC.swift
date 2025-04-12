2266abcc4cd2e1accf43b6c431e29f91486a4f18
Basem <basemxahmed@gmail.com>
2025-04-12 14:34:41 +0400

    Cleanup GFFollowerItemVC, add documentation comments

--- GHBook/Custom Views/ViewControlles/GFFollowerItemVC.swift
+++ GHBook/Custom Views/ViewControlles/GFFollowerItemVC.swift
@@ -8,22 +8,32 @@
 
 import UIKit
 
-
-
 class GFFollowerItemVC: GFItemInfoVC {
    
+    
+    //MARK: - Life Cycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureItems()
     }
     
+    
+    //MARK: - Configuration
+    
+    
     private func configureItems() {
         itemInfoOne.set(item: .followers, withCount: user.followers)
         itemInfoTwo.set(item: .following, withCount: user.following )
-        actionButton.set(backgroundColr: .systemGreen , title: "Get followers")
+        actionButton.set(backgroundColor: .systemGreen , title: "Get followers")
     }
    
     
+    //MARK: - Actions
+    
+    /// if the user has followers it Dismiss the view and notify the delegate
+    /// otherwise shows an alert
     override func actionButtonTapped() {
         guard user.followers != 0 else {
             presentGFAlertOnMainThread(title: "No Followers", message: "This user has no followers", buttonTitle: "Ok")
