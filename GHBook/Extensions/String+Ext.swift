774ac2b4f73ba331754d56d6d76b056e504f75c1
Basem <basemxahmed@gmail.com>
2025-04-14 13:07:05 +0400

    Cleanup String+Ext, add documentation comments

--- GHBook/Extensions/String+Ext.swift
+++ GHBook/Extensions/String+Ext.swift
@@ -9,20 +9,20 @@ import Foundation
 
 extension String {
     
+ 
+    // MARK: - Date Formatting
     
-        func formatteToGFdDate() -> String? {
-            let isoDateFormatter = ISO8601DateFormatter()
-                    isoDateFormatter.formatOptions = [.withInternetDateTime]
-
-                    // Parse the ISO date string to a Date object
-                    guard let date = isoDateFormatter.date(from: self) else { return nil }
-                    
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "MMM yyyy"  // Format to display Month and Year
-                    
-                    return dateFormatter.string(from: date)
-            
+    /// Converts an ISO8601 date string (e.g. "2021-04-07T12:34:56Z") to a formatted string like "Apr 2021"
+    func formatToGFDate() -> String? {
+        let isoDateFormatter = ISO8601DateFormatter()
+        isoDateFormatter.formatOptions = [.withInternetDateTime]
+        
+        // Parse the ISO date string to a Date object
+        guard let date = isoDateFormatter.date(from: self) else { return nil }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"  // Format to display Month and Year
+        
+        return dateFormatter.string(from: date)
     }
-    
-    
 }
