dce9b00e99e941a2c0899b187318e4ed6d4551c4
Basem <basemxahmed@gmail.com>
2025-04-14 10:49:52 +0400

    Cleanup GFRepoItemVC, Add documentation comments

--- GHBook/Custom Views/ViewControlles/GFRepoItemVC.swift
+++ GHBook/Custom Views/ViewControlles/GFRepoItemVC.swift
@@ -9,17 +9,26 @@ import UIKit
 
 class GFRepoItemVC: GFItemInfoVC {
    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureItems()
     }
     
+    
+    // MARK: - Configuration
+
+    
     private func configureItems() {
         itemInfoOne.set(item: .repos, withCount: user.publicRepos)
         itemInfoTwo.set(item: .gists, withCount: user.publicGists ?? 0)
-        actionButton.set(backgroundColr: .systemPurple, title: "Github Profile")
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
     }
     
+    
+    // MARK: - Actions
+
+    /// Notifies delegate to open the user's GitHub profile
     override func actionButtonTapped() {
         delegate.didTapGitHubProfile(for: user)
     }
