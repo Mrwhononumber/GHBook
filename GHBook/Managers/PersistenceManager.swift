e4d0ea428eb5f5d3612b4a114c4593beba61f321
Basem <basemxahmed@gmail.com>
2025-04-14 13:35:20 +0400

    Cleanup PersistanceManager, Add documentation comments

--- GHBook/Managers/PersistenceManager.swift
+++ GHBook/Managers/PersistenceManager.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 
+/// Represents the type of persistence action to perform
 enum PersistanceActionType {
     case add, remove
 }
 
-
+/// Manages storing and retrieving favorites using UserDefaults
 enum PersistenceManager {
     static private let defaults = UserDefaults.standard
     
     enum Keys {
-    static let favorites = "Favorites"
+        static let favorites = "Favorites"
     }
     
     
+    //MARK: - Favorites Persistance Methods
+    
+    /// Updates the favorites list by adding or removing a follower
     static func updateWith(favorite: Follower, actionType: PersistanceActionType, completed: @escaping (GFError?) -> Void) {
         retriveFavorites { result in
             switch result {
@@ -39,15 +43,14 @@ enum PersistenceManager {
                 
                 
                 completed(save(favorites: retrivedFavorites))
-                 
+                
             case .failure(let error):
                 completed(error )
             }
         }
-        
     }
     
-    
+    /// Retrieves the stored list of favorite followers
     static func retriveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
             completed(.success([]))
@@ -62,7 +65,7 @@ enum PersistenceManager {
         }
     }
     
-    
+    /// Saves the favorites array to UserDefaults
     static func save(favorites: [Follower]) -> GFError? {
         
         do {
