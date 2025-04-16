33229171bc09c53e04dee33480da33b2f642281d
Basem <basemxahmed@gmail.com>
2025-04-16 10:53:11 +0400

    Cleanup UIHelper, Add documentation comment

--- GHBook/Utilities/UIHelper.swift
+++ GHBook/Utilities/UIHelper.swift
@@ -9,6 +9,7 @@ import UIKit
 
 struct UIHelper {
     
+    /// Creates a UICollectionViewFlowLayout configured for a 3-column grid
     static func createThreeColumnFlowLayout(in view: UIView ) -> UICollectionViewFlowLayout {
         let width = view.bounds.width // The whole screen width
         let padding: CGFloat = 12
@@ -21,6 +22,5 @@ struct UIHelper {
         flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40 )
         
         return flowLayout
-        
     }
 }
