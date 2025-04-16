2ed9bfdd58e244f4c397185651430f9f7a3c2bc1
Basem <basemxahmed@gmail.com>
2025-04-16 10:56:00 +0400

    Cleanup Constants, Add documentation comments

--- GHBook/Utilities/Constants.swift
+++ GHBook/Utilities/Constants.swift
@@ -6,12 +6,18 @@
 //
 
 import Foundation
+import UIKit
 
+/// A collection of commonly used SF Symbols
 enum SFSymbols {
-    
-    static let location = "mappin.and.ellipse"
-    static let repos = "folder"
-    static let gists = "text.alignleft"
+    static let location  = "mappin.and.ellipse"
+    static let repos     = "folder"
+    static let gists     = "text.alignleft"
     static let followers = "heart"
-    static let following = "person.2 "
+    static let following = "person.2"
+}
+
+/// A collection of app image assets
+enum Images {
+    static let ghLogo = UIImage(named: "gh-logo")
 }
